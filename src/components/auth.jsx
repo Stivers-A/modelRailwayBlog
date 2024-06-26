5b41516e92671aacb650dc1d9aac835e0249f972
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-06-25 22:13:28 -0700

    getAuth(app) = auth somehow import { auth } from '../config/firebase' fails to import auth

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,6 +1,21 @@
+import { auth } from '../config/firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
 export const Auth = () => {
+    const [email,setEmail] = useState("") // blocks email and password inputs
+    const [password,setPassword] = useState("")
+
+    const signIn = async () =>{
+        //on button press
+        //async function as firebase tends to use 'promises'
+        await createUserWithEmailAndPassword(auth, email, password)
+    }
     return <div>
-         <input placeholder="Email"/>  
-         <input placeholder="Password"/>  
+         <input placeholder="Email" 
+             onChange={(e) => setEmail(e.target.value)}/> 
+         <input placeholder="Password"
+             onChange={(e) => setPassword(e.target.value)}/>  
+         <button onClick={signIn}> Sign In </button>
     </div>
+    
 }
