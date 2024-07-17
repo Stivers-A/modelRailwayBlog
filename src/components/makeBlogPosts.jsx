88ebbc1ea52d0a72351d78fe33f4e8fdd1b5ec17
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-07-16 22:28:47 -0700

    Did the last commit just fail to save? eitherway made a make blog post component and moved blogPosts to components need to make makeBlogPosts actually do what it says it does

---
+++ src/components/makeBlogPosts.jsx
@@ -0,0 +1,9 @@
+
+
+// currently exports to blog
+export const MakePost = () => {
+    <div>
+        <input placeholder="Post Title"/>
+        <input placeholder="Post Content"/>
+    </div>
+}
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,11 +1,13 @@
 import { Auth } from "../components/auth.jsx";
-import BlogPosts from './blogPosts.jsx'
+import  BlogPosts  from '../components/blogPosts.jsx'
+import  { MakePost }  from "../components/makeBlogPosts.jsx";
 
 export default function blog() {
 
     return (
         <div>
         <Auth />
+        <MakePost />
         <BlogPosts />
       </div>
 
