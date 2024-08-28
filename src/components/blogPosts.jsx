b581755fbbb067eb19220fa69fd440aaada1d6e5
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-08-28 06:55:13 -0700

    New photos are now tied to posts via their name

--- src/components/blogPosts.jsx
+++ src/components/blogPosts.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import { database, storage } from "../config/firebase.jsx";
 import { getDocs, collection,doc, deleteDoc,updateDoc } from 'firebase/firestore'
 import { ref,uploadBytes } from "firebase/storage";
-
+import { v4 } from "uuid" 
 //only one data base, thats the blog, everything else is hardcoded
 //includes rendering,editing and deleting
 //writeImageFunction imported into blogposts so it gets rendered per post 
-
+//v4 from uuid adds random strings to stuff to prevent repeat names
 
 //TODO add functionality to create and delete posts from specific accounts
 
@@ -16,11 +16,11 @@ export default function BlogPosts(){
 
      const uploadFile = () => {
         if (!fileUpload) return
-        const filesFolderRef = ref(storage, `blogPhotos/${fileUpload.name}` )
+        const filesFolderRef = ref(storage, `blogPhotos/${fileUpload.name + v4  ()}` )
         uploadBytes(filesFolderRef,fileUpload)
         //gives an error when try catched and set up as async, unsure whats up w/ that
-
-    } 
+        //TODO set file name to postID in firebase, so posts know which image to load and set up m 
+    }   //TODO MOVE TO makeBlogPost, add change image function to blogPosts
 
     //delete function TODO make deletion button visibility Account specific
     const deletePost = async (id) =>{
@@ -44,7 +44,7 @@ export default function BlogPosts(){
     }
 
     const[postList, setPostList] = useState([]);
-    //create list
+    //create list  
 
     const postCollectionRef = collection(database,"blogPosts")
     //collection Ref is used to to store blogposts that will be sent to data
--- src/components/makeBlogPost.jsx
+++ src/components/makeBlogPost.jsx
@@ -1,6 +1,9 @@
 import { addDoc,collection,deleteDoc,doc } from "firebase/firestore"
 import { useState } from "react"
-import { database, auth } from "../config/firebase.jsx";
+import { database,storage, auth } from "../config/firebase.jsx";
+import { v4 } from "uuid" 
+import { ref,uploadBytes } from "firebase/storage";
+
 
 
 
@@ -14,16 +17,25 @@ const postCollectionRef = collection(database,"blogPosts")
 export default function MakePost(){
     const [newPostTitle, setNewPostTitle] = useState("")
     const [newPostContent, setNewPostContent] = useState("")
+    const [fileUpload, setFileUpload] = useState(null)
+
     const onSubmitPost = async () =>{
+        if (!fileUpload) return
+        const imgName = newPostTitle + v4()
+        const filesFolderRef = ref(storage, `blogPhotos/${  (imgName)}` )
+            uploadBytes(filesFolderRef,fileUpload)
         try{
         await addDoc(postCollectionRef,{
             title: newPostTitle,
             postText: newPostContent,
-            userId: auth?.currentUser?.uid
+            userId: auth?.currentUser?.uid,
+            imageName: imgName
+            
             })
         }catch(err){
             console.error(err)
         }
+       
     }
 
 
@@ -32,6 +44,9 @@ export default function MakePost(){
     <div>   
         Title:  <input placeholder="Post Title" onChange={(e) => setNewPostTitle(e.target.value)}/>
         Content:  <input placeholder="Post Content" onChange={(e) => setNewPostContent(e.target.value)}/>
+        <div>   
+            <input type='file' onChange={(e) => setFileUpload(e.target.files[0])}/>
+        </div>
         <button onClick={onSubmitPost}>Submit Post</button>
     </div>
     )
