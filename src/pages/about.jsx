5e767e35bbeb488debbfd98d7452c6098edf5b64
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-09-09 07:18:51 -0700

    more detail in about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,3 +1,20 @@
 export default function about() {
-    return <h1>About</h1>
+    return (
+        
+    <div className=".container">
+    <h1>About</h1>
+    <div className="container-md">
+        <p>
+            This website is a project being worked on in parallel to my model train layout.
+            I designed it with the end goal of showing my model railway and having a show case of my knowledge about React and Firebase.
+            Thus this website is part passion project and part portfolio piece. 
+        </p>
+        <p>
+
+        </p>
+    </div>
+    </div>
+    //1st paragraph intro to site and goals
+    //2nd will be about the train set and specfics of the build
+    )
 }
