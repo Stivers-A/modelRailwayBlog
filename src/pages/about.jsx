fffccfa8eb8dc2acc12460aac36065fc360b45b7
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-10-29 22:10:55 -0700

    Formatting TODO: make images uploaded into posts also look nicer!

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,24 +1,23 @@
 export default function about() {
-    return (
-        
+  return (
     <div className=".container" id="buffer">
-        {/*
+      {/*
         <div className=".container-sm">
         <h1 id="title">About</h1>
         </div>
          */}
-    <div  className="container-md" id="buffer">
-        <p id='text'>
-            This website is a project being worked on in parallel to my model train layout.
-            I designed it with the end goal of showing my model railway and having a show case of my knowledge about React and Firebase.
-            Thus this website is part passion project and part portfolio piece. 
+      <div className="container-md" id="buffer">
+        <p id="text">
+          This website is a project being worked on in parallel to my model
+          train layout. I designed it with the end goal of showing my model
+          railway and having a show case of my knowledge about React and
+          Firebase. Thus this website is part passion project and part portfolio
+          piece.
         </p>
-        <p>
-
-        </p>
-    </div>
+        <p></p>
+      </div>
     </div>
     //1st paragraph intro to site and goals
     //2nd will be about the train set and specfics of the build
-    )
+  );
 }
